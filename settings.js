9150353bddd0d9a1e388b1217f1177564609f579
∆RY∆N-X tech <aryantech961@gmail.com>
2026-06-30 11:53:40 +0300

    Aryan

---
+++ settings.js
@@ -0,0 +1,25 @@
+
+/**
+* supreme
+* caspar x
+*/
+
+const settings = {
+  packname: '✮⃝∆RYAN',
+  author: '✮⃝𝐒ᵘᵖʳᵉᵐᵉ',
+  botName: "∆RY∆N-𝐗 𝐛𝐨𝐭",
+  botOwner: '✮⃝𝐒ᵘᵖʳᵉᵐᵉ', // Your name
+  ownerNumber: process.env.OWNER_NUMBER || '255772336533', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.7.6",
+  updateBranch: 'main',
+  updateZipToken: '',
+  updateZipUrl: "https://github.com/dot-666/x/archive/refs/heads/main.zip",
+  timezone: 'Africa/Nairobi',
+};
+
+module.exports = settings;
