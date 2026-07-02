1d9de5851a152a1cf0e4f9ee98490e8cfd3d046d
∆RY∆N-X tech <aryantech961@gmail.com>
2026-07-02 11:00:17 +0300

    Update main.js

--- main.js
+++ main.js
@@ -568,7 +568,7 @@ const fake = createFakeContact(message);
             const datez = moment(Date.now()).tz(timezones).format("DD/MM/YYYY");
 
             if (message.message) {
-                lolcatjs.fromString(`┏━━━━━━━━━━━━━『  JUNE-X BOT 』━━━━━━━━━━━━━─`);
+                lolcatjs.fromString(`┏━━━━━━━━━━━━━『  ∆RY∆N-X ULUTRA 』━━━━━━━━━━━━━─`);
                 lolcatjs.fromString(`»  Sent Time: ${dayz}, ${timez}`);
                 lolcatjs.fromString(`»  Date: ${datez}`);
                 lolcatjs.fromString(`»  Message Type: ${mtype}`);
